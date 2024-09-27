1f038e2ac5636f5fd9bebe39cd585bdbf8b86e37
Jigyashu Kumar Gupta <75210289+jigyashu765@users.noreply.github.com>
2024-09-27 18:26:04 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,50 @@
+// variable are container for storing data like (Interger, String, Boolen, Folt )  
+
+// JavaScript is a dyanimic type languge. 
+
+// keyword varibale name       value
+//     var       a         =     123;
+
+// // console.log(a);
+// console.log(typeof(a) +  " " + a);
+
+// a = "Coding Desk";
+
+// console.log(typeof(a) +  " " + a);
+
+
+var a = 20; //number
+
+var b = 20.5; //number
+
+var c; //undefine
+            //1234567891011
+var MyName = 'Coding Desk'; //String
+            //012345678910
+
+MyName = 1234;
+
+var WorkDone = true; //Boolean
+
+// console.log(typeof(a) +  " " + a);
+// console.log(typeof(b) +  " " + b);
+// console.log(typeof(c) +  " " + c);
+// console.log(typeof(MyName) +  " " + MyName);
+console.log(typeof(MyName) +  " " + MyName.toUpperCase());//CODING DESK
+console.log(typeof(MyName) +  " " + MyName.toLowerCase());//coding desk
+console.log(typeof(MyName) +  " " + MyName.length);//11; it retrun the lenth of String.
+console.log(typeof(MyName) +  " " + MyName.trim());//Coding Desk without space.
+console.log(typeof(MyName) +  " " + MyName.indexOf("D"));//Coding Desk without space.
+
+// console.log(typeof(WorkDone) +  " " + WorkDone);
+
+
+
+// Rules to delclare a varibale;
+
+// Name contain leeter Number, undersore, and dollor sign; e.g myname, MyName, MyName1, My_Name, _MyName, My$Name.  1MyName❌
+// Name must be strat from  letter.
+// Name can  also have _ and $ sign.
+// Name is case sensetive; A and a both are diffrent
+// resved keyword cannot be used.
+
